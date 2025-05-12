772c4c508fd70dca5924b7d9bdb1062cc86191d8
pedrickyst_ <pedricky63@gmail.com>
2025-05-12 16:42:44 +0000

    teste 1.1

--- areadeestudo.c
+++ areadeestudo.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 
 int main() {
+
+    char nome1[10] = "Descarga";
+    char nome2[10] = "Etiquetagem";
+    char nome3[10] = "Conferencia";
+    char nome4[10] = "Guarda";
     int usuario;
-    double Descarga = 1.11;
-    double Etiquetagem = 1.46;
-    double Conferencia = 0.55;
-    double Guarda = 2.50;
+    float Descarga = 1.11;
+    float Etiquetagem = 1.46;
+    float Conferencia = 0.55;
+    float Guarda = 2.50;
 
     printf("Digite o Número de PALETES: ");
     scanf("%d", &usuario);
     
-    if (usuario <= Descarga)
-    {
-        printf("%d = %d %f\n", usuario * Descarga) % 2;
-    }
+    printf("%s = %d %f\n", nome1, usuario * Descarga) % 60;
+    printf("%s = %d %f\n", nome2, usuario * Etiquetagem) % 60;
+    printf("%s = %d %f\n", nome3, usuario * Conferencia) % 60;
+    printf("%s = %d %f\n", nome4, usuario * Guarda) % 60;
     
+    
+    return 0;
 }
